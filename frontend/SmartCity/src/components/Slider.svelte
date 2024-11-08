260816e68d966dc8069b4c26bd4a5910e2b2ce0a
qwaqd <qwaq.dev@gmail.com>
2024-11-09 01:20:56 +0500

    Create Header and slider(non functional)

---
+++ frontend/SmartCity/src/components/Slider.svelte
@@ -0,0 +1,84 @@
+<script>
+    import slide1 from '../lib/imgs/slider/slider1.png';
+    import right from '../lib/imgs/right.png';
+</script>
+
+<div class="slider">
+    <img src={right} alt="" class="left">
+    <img src={right} alt="" class="right">
+    <div class="slider-item">
+        <div class="slider-title">
+            <h2 class="title-text">Новости города</h2>
+            <div class="border"></div>
+        </div>
+        <div class="slider-images">
+            <img src={slide1} alt="">
+        </div>
+        <div class="slider-text">
+            <h2 class="slider-suptitle">Изменение схемы светофоров на проспекте Абая</h2>
+            <p class="slider-p">Проект нужен для обезопашивания пешеходов - будет добавлен целый светофор для того, чтобы люди...</p>
+        </div>
+    </div>
+</div>
+
+<style>
+    .left {
+        margin-top: 200px;
+        position: absolute;
+    }
+
+    img:hover {
+        cursor: pointer;
+    }
+
+    .right{
+        margin-left: 520px;
+        margin-top: 200px;
+        transform: rotate(180deg);
+        position: absolute;
+    }
+
+    .slider-item {
+        margin: 0 5vw;
+    }
+
+    .slider {
+        margin-top: 40vh;
+        padding: 35px;
+        position: relative;
+        width: 640px;
+        height: 422px;
+        background-image: url('../lib/imgs/slider-bg.png');
+        object-fit: contain; /* Сохраняем пропорции изображения, заполняя контейнер */
+        background-size: cover;
+    }
+
+    .slider-title {
+        font-size: 1.5vh;
+    }
+
+    .border {
+        margin: 0;
+        background-color: #761596;
+        height: 3px;
+        width: 50px;
+    }
+
+    .title-text {
+        margin: 0;
+    }
+
+    .slider-images {
+        margin: 3vh 0 0 0;
+    }
+
+    .slider-text {
+        width: 400px;
+    }
+
+    .slider-suptitle {
+        margin: 10px;
+        font-weight: 400;
+        font-size: 1.7vh;
+    }
+</style>
--- frontend/SmartCity/src/routes/+page.svelte
+++ frontend/SmartCity/src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Header from "../components/Header.svelte";
+    import Slider from "../components/Slider.svelte";
+
+</script>
+
+<div class="container">
+    <div class="bg-img">
+        <Header/>
+        <Slider/>
+    </div>
+</div>
+
+<style>
+    .container {
+        width: auto;
+        overflow: hidden;
+        height: 200vh;
+        background: rgb(201,198,255);
+        background: linear-gradient(90deg, rgba(201,198,255,1) 0%, rgba(142,158,244,0.6643032212885154) 80%);
+    }
+
+    .bg-img {
+        height: 80vh;
+        width: 100vw;
+        background-image: url('../lib/imgs/bg-img.jpg');
+        object-fit: contain; /* Сохраняем пропорции изображения, заполняя контейнер */
+        background-size: cover;
+    }
+</style>
