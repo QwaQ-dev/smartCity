c42498777841b4f048eea78439a653e37dd9dcbe
qwaqd <qwaq.dev@gmail.com>
2024-11-10 12:04:15 +0500

    a

--- frontend/SmartCity/svelte.config.js
+++ frontend/SmartCity/svelte.config.js
@@ -1,12 +1,14 @@
 import adapter from '@sveltejs/adapter-auto';
 
-/** @type {import('@sveltejs/kit').Config} */
+import('@sveltejs/kit').Config
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		paths: {
+			base: '/smartCity',
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'src',
 	}
 };
 
